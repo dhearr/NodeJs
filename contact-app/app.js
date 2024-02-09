55e663c7e393717db478e809752f12a2a38b108b
dhearr <dhearr27@gmail.com>
2024-02-09 18:01:56 +0700

    Contact-App done

--- contact-app/app.js
+++ contact-app/app.js
@@ -1,30 +1,75 @@
 const yargs = require("yargs");
-const { saveContacts } = require("./contacts");
+const {
+  saveContacts,
+  listContact,
+  detailContact,
+  deleteContact,
+} = require("./contacts");
 
+// Add Contact
+yargs
+  .command(
+    "add",
+    "Add New Contact",
+    (builder) => {
+      builder
+        .option("name", {
+          describe: "Full Name",
+          demandOption: true,
+          type: "string",
+        })
+        .option("email", {
+          describe: "Email",
+          demandOption: false,
+          type: "string",
+        })
+
+        .option("phone", {
+          describe: "Phone Number",
+          demandOption: true,
+          type: "string",
+        });
+    },
+    (argv) => {
+      saveContacts(argv.name, argv.email, argv.phone);
+    }
+  )
+  .demandCommand();
+
+// View all list name & phone number contact
+yargs.command("list", "View all list name & phone number contact", () => {
+  listContact();
+});
+
+// Show contact details
 yargs.command(
-  "add",
-  "Add New Contact",
+  "detail",
+  "Show contact details by name",
   (builder) => {
-    builder
-      .option("name", {
-        describe: "Full Name",
-        demandOption: true,
-        type: "string",
-      })
-      .option("email", {
-        describe: "Email",
-        demandOption: false,
-        type: "string",
-      })
-
-      .option("phone", {
-        describe: "Phone Number",
-        demandOption: true,
-        type: "string",
-      });
+    builder.option("name", {
+      describe: "Full Name",
+      demandOption: true,
+      type: "string",
+    });
   },
   (argv) => {
-    saveContacts(argv.name, argv.email, argv.phone);
+    detailContact(argv.name);
+  }
+);
+
+// Delete contact by name
+yargs.command(
+  "delete",
+  "Delete contact by name",
+  (builder) => {
+    builder.option("name", {
+      describe: "Full Name",
+      demandOption: true,
+      type: "string",
+    });
+  },
+  (argv) => {
+    deleteContact(argv.name);
   }
 );
 
--- contact-app/contacts.js
+++ contact-app/contacts.js
@@ -14,16 +14,21 @@ if (!fs.existsSync(dataPath)) {
   fs.writeFileSync(dataPath, "[]", "utf-8");
 }
 
+// Loadt Contact
+const loadContact = () => {
+  const file = fs.readFileSync(dataPath, "utf-8");
+  const contacts = JSON.parse(file);
+  return contacts;
+};
+
 // Save Contacts
 const saveContacts = (name, email, phone) => {
   const contact = { name, email, phone };
-
-  const file = fs.readFileSync(dataPath, "utf-8");
-  const contacts = JSON.parse(file);
+  const contacts = loadContact();
 
   // cek duplicate name
   const duplicate = contacts.find((contact) => contact.name === name);
-  if (duplicate !== undefined) {
+  if (duplicate) {
     console.log(
       chalk.red.inverse.underline(
         `Contact with the name ${name} is already registered, use another name`
@@ -64,4 +69,53 @@ const saveContacts = (name, email, phone) => {
   );
 };
 
-module.exports = { saveContacts };
+// List Contact
+const listContact = () => {
+  const contacts = loadContact();
+  console.log(chalk.cyan.inverse("Contact List : "));
+  contacts.forEach((contact, i) => {
+    console.log(
+      chalk.underline(`${i + 1}. ${contact.name} - ${contact.phone}`)
+    );
+  });
+};
+
+// Detail Contact
+const detailContact = (name) => {
+  const contacts = loadContact();
+  const contact = contacts.find(
+    (data) => data.name.toLowerCase() === name.toLowerCase()
+  );
+  if (!contact) {
+    console.log(
+      chalk.red.inverse.underline(`Contact with name ${name} not found`)
+    );
+    return;
+  }
+  console.log(chalk.cyan.inverse(`Detail Contact : `));
+  console.log(`Name : ${contact.name}`);
+  console.log(`Phone Number : ${contact.phone}`);
+  if (contact.email) {
+    console.log(`Email : ${contact.email}`);
+  }
+};
+
+// Delete contact
+const deleteContact = (name) => {
+  const contacts = loadContact();
+  const newContact = contacts.filter(
+    (contact) => contact.name.toLowerCase() !== name.toLowerCase()
+  );
+  if (contacts.length === newContact.length) {
+    console.log(
+      chalk.red.inverse.underline(`Contact with name ${name} not found!`)
+    );
+    return;
+  }
+  fs.writeFileSync(dataPath, JSON.stringify(newContact));
+  console.log(
+    chalk.green.inverse.underline(`Contact ${name} has been deleted!`)
+  );
+};
+
+module.exports = { saveContacts, listContact, detailContact, deleteContact };
